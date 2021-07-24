8aff3b99755a5be6e49ab7d78d7cf023c809776a
Mark Hartmann <contact@mark-hartmann.com>
2021-07-25 00:10:19 +0200

    created helper function that sets up a new exec.Cmd

--- misc.go
+++ misc.go
@@ -9,13 +9,26 @@ import (
 // Installed returns whether VeraCrypt is installed or not, as well es the installed
 // version, e.g VeraCrypt 1.24-Update7
 func Installed() (string, bool) {
-	cmd := exec.Command("veracrypt", "-t", "--version")
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-
+	cmd, stdout, _ := newCommand("-t", "--version")
+	// cmd.Run will return "veracrypt: command not found" if veracrypt is not
+	// installed on this system
 	if err := cmd.Run(); err != nil {
 		return "", false
 	}
 
+	// the stdout content has a newline, so we simply trim it away
 	return strings.Trim(stdout.String(), "\n"), true
+}
+
+// newCommand returns a "pre configured" exec.Cmd plus the stdout and stderr buffers
+// used to intercept command line errors and output
+func newCommand(args ...string) (cmd *exec.Cmd, stdout, stderr *bytes.Buffer) {
+	stdout = &bytes.Buffer{}
+	stderr = &bytes.Buffer{}
+
+	cmd = exec.Command("veracrypt", args...)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	return
 }
